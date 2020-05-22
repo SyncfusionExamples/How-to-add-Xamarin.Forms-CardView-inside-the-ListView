b12313a9c31dcbf1f99c88b591877098b35dcd3c
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-05-22 16:52:29 +0530

    Sample included

---
+++ Cards_ListView/Cards_ListView/CardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cards_ListView
+{
+    public class CardModel
+    {
+        public string Title { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
